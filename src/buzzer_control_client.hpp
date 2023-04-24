72a9b5234fde3617670d907bb3bd44e2caea52c8
Ben <ben.quan@vertiq.co>
2023-04-24 18:12:06 -0400

    Update and add new client files for 81-08, Fortiq, and Pulsing Modules

--- src/buzzer_control_client.hpp
+++ src/buzzer_control_client.hpp
@@ -3,22 +3,11 @@
 
   This file is part of the IQ C++ API.
 
-  IQ C++ API is free software: you can redistribute it and/or modify
-  it under the terms of the GNU Lesser General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  IQ C++ API is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  GNU Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public License
-  along with this program. If not, see <http://www.gnu.org/licenses/>.
+  This code is licensed under the MIT license (see LICENSE or https://opensource.org/licenses/MIT for details)
 */
 
 /*
-  Name: buffered_voltage_monitor_client.hpp
+  Name: buzzer_control_client.hpp
   Last update: 3/7/2019 by Raphael Van Hoffelen
   Author: Matthew Piccoli
   Contributors: Raphael Van Hoffelen
