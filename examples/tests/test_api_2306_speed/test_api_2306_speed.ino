d47f1c1f48382ae025288243dcba228c4885d631
Ben <ben.quan@vertiq.co>
2025-01-31 15:14:03 -0500

    added telemetry_style_ to uavcan_node_client.hpp

--- examples/tests/test_api_2306_speed/test_api_2306_speed.ino
+++ examples/tests/test_api_2306_speed/test_api_2306_speed.ino
@@ -7,10 +7,10 @@ BrushlessDriveClient bdrive(0);
 CoilTemperatureEstimatorClient coilTemperatureEstimator(0);
 AnticoggingProClient anticog_pro(0);
 MagAlphaClient mag_alpha(0);
-StowUserInterfaceClient stowUserInterface(0);
+// StowUserInterfaceClient stowUserInterface(0);
 // ArmingHandlerClient armingHandler(0);                        // Initialize Arming Handler Client
-PowerMonitorClient powerMonitorClient(0);                    // Initialize Power Monitor Client
-// UavcanNodeClient uavcanNode(0);                              // Initialize UAVCAN Node Client
+// PowerMonitorClient powerMonitorClient(0);                    // Initialize Power Monitor Client
+UavcanNodeClient uavcanNode(0);                              // Initialize UAVCAN Node Client
 
 
 void setup() {
@@ -23,9 +23,10 @@ void setup() {
     uint8_t num_harmonics = 0;
     float angle_rad = 0.0;
     // uint32_t consecutive_disarming_throttles_to_disarm = 0;
-    // uint8_t arming_by_arming_status = 0;
-    float volts_cascaded = 0.0;
-    uint32_t volts_cascaded_filter_fc = 0;
+    uint8_t arming_by_arming_status = 0;
+    uint8_t telemetry_style = 0;
+    // float volts_cascaded = 0.0;
+    // uint32_t volts_cascaded_filter_fc = 0;
 
     // Set up serial port
     ser.begin(115200);
@@ -91,37 +92,49 @@ void setup() {
     //   Serial.println(); 
     // }
 
-    // if (ser.get(uavcanNode.arming_by_arming_status_, arming_by_arming_status)){
-    //   Serial.print("disarming by arming status: ");
-    //   Serial.print(arming_by_arming_status);
+    if (ser.get(uavcanNode.arming_by_arming_status_, arming_by_arming_status)){
+      Serial.print("disarming by arming status: ");
+      Serial.print(arming_by_arming_status);
+      Serial.println(); 
+    }
+
+    if (ser.get(uavcanNode.arming_by_arming_status_, arming_by_arming_status)){
+      Serial.print("disarming by arming status: ");
+      Serial.print(arming_by_arming_status);
+      Serial.println(); 
+    }
+
+    if (ser.get(uavcanNode.telemetry_style_, telemetry_style)){
+      Serial.print("telemetry style: ");
+      Serial.print(telemetry_style);
+      Serial.println(); 
+    }
+
+    // if (ser.get(powerMonitorClient.volts_cascaded_, volts_cascaded)){
+    //   Serial.print("volts cascaded: ");
+    //   Serial.print(volts_cascaded);
     //   Serial.println(); 
     // }
 
-    if (ser.get(powerMonitorClient.volts_cascaded_, volts_cascaded)){
-      Serial.print("volts cascaded: ");
-      Serial.print(volts_cascaded);
-      Serial.println(); 
-    }
-
-    if (ser.get(powerMonitorClient.volts_cascaded_filter_fc_, volts_cascaded_filter_fc)){
-      Serial.print("volts cascaded filter fc: ");
-      Serial.print(volts_cascaded_filter_fc);
-      Serial.println(); 
-    }
+    // if (ser.get(powerMonitorClient.volts_cascaded_filter_fc_, volts_cascaded_filter_fc)){
+    //   Serial.print("volts cascaded filter fc: ");
+    //   Serial.print(volts_cascaded_filter_fc);
+    //   Serial.println(); 
+    // }
 }
 
 void loop() {
     // put your main code here, to run repeatedly:
-    float volts = 1.0;
-    ser.set(bdrive.drive_spin_volts_, volts);
+    // float volts = 1.0;
+    // ser.set(bdrive.drive_spin_volts_, volts);
 
-    delay(5000);
-    ser.set(bdrive.drive_coast_);
+    // delay(5000);
+    // ser.set(bdrive.drive_coast_);
 
-    delay(1000);
+    // delay(1000);
 
-    ser.set(stowUserInterface.stow_);
+    // ser.set(stowUserInterface.stow_);
 
-    delay(1000);
+    // delay(1000);
 
 }
